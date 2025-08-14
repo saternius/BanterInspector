2721af864649ab655cf09666608909d22e8f4866
saternius <satern693@gmail.com>
2025-08-14 07:49:23 -0500

    I can not use vscode to code instead

---
+++ microservices/linker/update_on_save.py
@@ -0,0 +1,20 @@
+import time
+import os
+import sys
+import requests
+
+linker_port = 5005
+
+
+print(sys.argv)
+print("Inventory Link: " + os.getcwd())
+cur_path = f"{os.getcwd()}/microservices/linker"
+target_path = sys.argv[1]
+if(target_path.startswith(cur_path)):
+    target_path = target_path[len(cur_path)+1:]
+    print("TargetPath: " + target_path)
+else:
+    print("TargetPath: " + target_path + " is not in the current path: " + cur_path)
+    exit()
+
+requests.get(f"http://localhost:{linker_port}/save?file={target_path}")
