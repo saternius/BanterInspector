5d308e5d83e4d61ac4035414dfc49fbd52e96271
saternius <satern693@gmail.com>
2025-10-29 22:24:36 -0500

    transformed

--- prompts/builder/extensions/code_linker/inventory/Technocrat/Scripts/Trackers.js
+++ prompts/builder/extensions/code_linker/inventory/Technocrat/Scripts/Trackers.js
@@ -1,4 +1,20 @@
 log("Trackers", "Trackers script loaded");
+const attachmentPoints = {
+    HEAD: 0,
+    BODY: 1,
+    LEFT_HAND: 2,
+    RIGHT_HAND: 3,
+    COCKPIT: 4,
+}
+
+function hexToRgb(hex) {
+    const [r, g, b] = hex.match(/\w\w/g).map(c => parseInt(c, 16) / 255);
+    return {r, g, b, a: 1};
+}
+
+let me = SM.myName();
+let color = scene.localUser.color;
+let rgba = hexToRgb(color);
 
 window.GetTracker = async (name)=>{
     let tracker = SM.getEntityById(`People/${me}/Trackers/${name}`, false);
@@ -11,106 +27,26 @@ window.GetTracker = async (name)=>{
     }
 }
 
-const attachmentPoints = {
-    HEAD: 0,
-    BODY: 1,
-    LEFT_HAND: 2,
-    RIGHT_HAND: 3,
-    COCKPIT: 4,
-}
-
-let me = SM.myName();
-let color = scene.localUser.color;
-
-//function to convert hex to rgb
-function hexToRgb(hex) {
-    const [r, g, b] = hex.match(/\w\w/g).map(c => parseInt(c, 16) / 255);
-    return {r, g, b, a: 1};
-}
-
-let rgba = hexToRgb(color);
-
-log("Trackers", "color", rgba);
 
 let getOrMakeTracker = async (name)=>{
     let tracker = await LoadItem('Tracker', `People/${me}/Trackers`, {name: name});
     let material = tracker.getComponent("Material");
-    log("Trackers", "Material found, setting color", rgba);
     material.Set("color", rgba);
-    log("Trackers", "Attachment found, setting attachment point", attachmentPoints[name]);
     let attachment = tracker.getComponent("AttachedObject");
     attachment.Set("attachmentPoint", attachmentPoints[name]);
     attachment.Set("uid", scene.localUser.uid);
+    log("Trackers", `${name} tracker loaded`);
     return tracker;
 }
 
-async function VerifyComponentExistance(path){
-    let entKey = `$${path}:active`
-    log("Trackers", "Verifying existence of", path, "with key", entKey, "and value", networking.spaceState[entKey]);
-    if(networking.spaceState[entKey]){
-        return true;
-    }
-    
-    await new Promise(resolve => setTimeout(resolve, 500));
-    return await VerifyExistance(path);
-}
 
-
-async function VerifyExistance(path){
-    let entKey = `$${path}:active`
-    log("Trackers", "Verifying existence of", path, "with key", entKey, "and value", networking.spaceState[entKey]);
-    if(networking.spaceState[entKey]){
-        return true;
-    }
-    
-    await new Promise(resolve => setTimeout(resolve, 500));
-    return await VerifyExistance(path);
-}
-
-async function VerifyInexistance(path){
-    let entKey = `$${path}:active`
-    log("Trackers", "Verifying inexistence of", path, "with key", entKey, "and value", networking.spaceState[entKey]);
-    if(!networking.spaceState[entKey] || networking.spaceState[entKey] === "null"){
-        return true;
-    }
-    await RemoveEntity(path);
-    await new Promise(resolve => setTimeout(resolve, 500));
-    return await VerifyInexistance(path);
-}
-
-let headTracker = null;
-let leftHandTracker = null;
-let rightHandTracker = null;
-
-//conditions
-// it doesnt exist in spaceprops, but exist in local
-// it exists in spaceprops, but not in local
-// it exists in spaceprops and local
-// it exists in neither
-
-
-// (async ()=>{
-//     log("Trackers", "[START]");
-//     await VerifyExistance("People");
-
-//     log("Trackers", "'People' exists, checking my dir..", me);
-
-//     await RemoveEntity("People/"+me);
-//     await VerifyInexistance("People/"+me);
-//     await AddEntity("People", me);
-//     await VerifyExistance("People/"+me);
-//     await AddEntity("People/"+me, "Trackers");
-//     await VerifyExistance("People/"+me+"/Trackers");
-
-//     log("Trackers", "$People/${me}/Trackers exists, loading trackers..");
-
-
-//     headTracker = await getOrMakeTracker("HEAD");
-//     log("Trackers", "HEAD tracker loaded");
-//     leftHandTracker = await getOrMakeTracker("LEFT_HAND");
-//     log("Trackers", "LEFT_HAND tracker loaded");
-//     rightHandTracker = await getOrMakeTracker("RIGHT_HAND");
-//     log("Trackers", "RIGHT_HAND tracker loaded");
-
-//     log("Trackers", "[END]");
-// })()
+(async ()=>{
+    log("Trackers", "[START]");
+    await RemoveEntity("People/"+me);
+    await AddEntity("People", me);
+    await AddEntity("People/"+me, "Trackers");
+    await getOrMakeTracker("HEAD");
+    await getOrMakeTracker("LEFT_HAND");
+    await getOrMakeTracker("RIGHT_HAND");
+    log("Trackers", "[END]");
+})()
